29fe1721336bfe2877c5c169efbaa242cdc6c4d8
Cori Caridine <caridine.corin@gmail.com>
2019-01-08 12:13:33 -0600

    backup initial commit

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-console.log('oh hai');
-console.log($);
+// console.log('oh hai')
+// console.log($)
+$(() => {
+  $('form').on('submit', (event) => {
+    event.preventDefault()
+
+    const userInput = $('input[type="text"]').val()
+
+    $.ajax({
+      url: 'http://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput
+    }).then(
+      (data) => {
+        $('#title').html(data.Title)
+        $('#year').html(data.Year)
+        $('#rated').html(data.Rated)
+      },
+      () => {
+        console.log('bad request')
+      }
+    )
+  })
+})
