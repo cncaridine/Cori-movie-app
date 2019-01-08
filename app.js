c56d034f09e3745aa3adacf7ab1c4f183d7072f5
Cori Caridine <caridine.corin@gmail.com>
2019-01-08 12:56:11 -0600

    able to generate first matching title

--- app.js
+++ app.js
@@ -5,14 +5,22 @@ $(() => {
     event.preventDefault()
 
     const userInput = $('input[type="text"]').val()
-
+    // =============user will enter the name of a title=================
     $.ajax({
       url: 'http://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput
     }).then(
       (data) => {
+        // ========generate a list with all of the key words from input =======
         $('#title').html(data.Title)
         $('#year').html(data.Year)
-        $('#rated').html(data.Rated)
+
+        // ======== make titles clickable ==================
+
+        // ========info to show in a modal ================
+        // $('#title').html(data.Title)
+        // $('#year').html(data.Year)
+        // $('#rated').html(data.Rated)
+        // $('#plot').html(data.Plot)
       },
       () => {
         console.log('bad request')
